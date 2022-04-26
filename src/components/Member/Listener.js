b6ac54a0185664cb1306b888bccf0edf30b1b025
freeplanets <freeplanets@gmail.com>
2022-04-26 15:08:29 +0800

    mbrIF , 'switch' to ' moveleft or moveright

--- src/components/Member/Listener.js
+++ src/components/Member/Listener.js
@@ -23,6 +23,50 @@ function dragOver(e) {
 function dragLeave() {
 	this.classList.remove('hover');
 }
+function moveRight(e, targetid) {
+  const dropTargets = document.querySelectorAll('[data-role="drag-drop-container"]');
+  let moveMark = false;
+  let isNext = false;
+  let isLast = false;
+  let preChild;
+  dropTargets.forEach((target) => {
+    if (target.id === targetid) {
+      if (target.firstElementChild) {
+        moveMark = true;
+      }
+    } else {
+      if (moveMark) isNext = true;
+      if (!target.firstElementChild) isLast = true;
+    }
+    if (moveMark && isNext) {
+      e.dataTransfer.setData('text/plain', preChild.id);
+      target.appendChild(preChild);
+    }
+    if (isLast) moveMark = false;
+    preChild = target.firstElementChild;
+  });
+}
+function moveLeft(e, sourceid, targetid) {
+  const dropTargets = document.querySelectorAll('[data-role="drag-drop-container"]');
+  let moveMark = false;
+  let isPrev = false;
+  let nextChild;
+  let PrevID;
+  dropTargets.forEach((target) => {
+    nextChild = target.firstElementChild;
+    if (target.id === sourceid) {
+      isPrev = true;
+    } else if (isPrev) {
+      moveMark = true;
+    }
+    if (target.id === targetid) moveMark = false;
+    if (isPrev && moveMark) {
+      e.dataTransfer.setData('text/plain', nextChild.id);
+      document.querySelector(`#${PrevID}`).appendChild(nextChild);
+    }
+    PrevID = target.id;
+  });
+}
 function dropped(e) {
   // execute function only when target container is different from source container
   if (this.id !== sourceContainerId) {
@@ -36,8 +80,13 @@ function dropped(e) {
 		document.querySelector(`#${this.id}`).appendChild(document.querySelector(`#${id}`));
     // e.target.appendChild(document.querySelector(`#${id}`));
     if (moveold) {
-      e.dataTransfer.setData('text/plain', this.firstElementChild.id);
-      document.querySelector(`#${sourceContainerId}`).appendChild(this.firstElementChild);
+      if (this.id > sourceContainerId) {
+        moveLeft(e, sourceContainerId, this.id);
+      } else {
+        moveRight(e, this.id);
+      }
+      // e.dataTransfer.setData('text/plain', this.firstElementChild.id);
+      // document.querySelector(`#${sourceContainerId}`).appendChild(this.firstElementChild);
     }
   }
 }
